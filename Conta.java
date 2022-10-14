82be2976f4e595b6e7650f9afbfca31cd98323d6
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 18:51:57 -0300

    adicionando metodo transfere

--- Conta.java
+++ Conta.java
@@ -11,4 +11,21 @@ public class Conta {
     public void deposita(double valor) {
         this.saldo += valor;
     }
+
+    public boolean saca(double valor) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean transfere(double valor, Conta destino) {
+        boolean deuCerto = this.saca(valor);
+        if (deuCerto) {
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 }
--- CriaConta.java
+++ CriaConta.java
@@ -7,9 +7,12 @@ public class CriaConta {
 
     public static void main(String[] args) {
         Conta minhaConta = new Conta();
+        Conta outraConta = new Conta();
         minhaConta.deposita(200);
         minhaConta.deposita(10);
+        minhaConta.transfere(100, outraConta);
 
         System.out.println(minhaConta.saldo);
+        System.out.println(outraConta.saldo);
     }
 }
