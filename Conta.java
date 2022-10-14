cd02dbb6af28ee32d5ae3859b66bf9c235a5ef45
Michel Lopes <lopesmichel489@gmail.com>
2022-10-14 18:07:38 -0300

    instanciando classe Conta

---
+++ Conta.java
@@ -0,0 +1,10 @@
+/**
+ * Conta
+ */
+public class Conta {
+
+    double saldo;
+    int agencia;
+    int numero_conta;
+    String titular;
+}
---
+++ CriaConta.java
@@ -0,0 +1,14 @@
+
+
+/**
+ * CriaConta
+ */
+public class CriaConta {
+
+    public static void main(String[] args) {
+        Conta minhaConta = new Conta();
+        minhaConta.saldo = 200;
+
+        System.out.println(minhaConta.saldo);
+    }
+}
